fd055af1b03b41d5dff4f6c62146f5947edaa620
Захаров Артем <zaharov-art@mail.ru>
2023-05-23 18:56:39 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication26/ConsoleApplication26.cpp
@@ -0,0 +1,20 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main() {
+	char c;
+	c = getchar();
+	while (c != EOF) {
+		if (c == ' ') {
+			putchar('\t');
+		}
+		else{
+			putchar(c);
+		}
+		c = getchar();
+	}
+	
+
+}
+
